82009def954ce1a0a9b137a4010522fc8647a0f7
yizhen05 <s22056@tokyo.kosen-ac.jp>
2024-06-01 22:08:09 +0900

    first commit

---
+++ B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int N, M;
+	cin >> N >> M;
+	vector<int>         A(M);
+	vector<vector<int>> X(N, vector<int>(M));
+	for (int i = 0; i < M; i++)
+	{
+		cin >> A[i];
+	}
+
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			cin >> X[i][j];
+		}
+	}
+
+	vector<int> intake(M, 0);
+	for (int i = 0; i < N; i++)
+	{
+		for (int j = 0; j < M; j++)
+		{
+			intake[j] += X[i][j];
+		}
+	}
+
+	for (int i = 0; i < M; i++)
+	{
+		if (intake[i] < A[i])
+		{
+			cout << "No" << endl;
+			return 0;
+		}
+	}
+	cout << "Yes" << endl;
+	return 0;
+}
