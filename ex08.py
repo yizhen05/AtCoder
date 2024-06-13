d59c2e48e3c1f58af9f6fb0948ba96d6253407ad
yizhen05 <s22056@tokyo.kosen-ac.jp>
2024-06-13 19:12:54 +0900

    kkkjjxxcvbhjmkxctvu

---
+++ ex08.py
@@ -0,0 +1,8 @@
+N = int(input())
+i = 0
+while i < N:
+    a, b = input().split()
+    a = int(a)
+    b = int(b)
+    print(a + b)
+    i += 1
---
+++ ex09.py
@@ -0,0 +1,5 @@
+N = int(input())
+A = int(input())
+for i in range(N):
+    a, b = input().split()
+    b = int(b)
