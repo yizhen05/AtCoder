a0e248e38776b11d86e7e05fcd72a1db3fdcd367
yizhen05 <s22056@tokyo.kosen-ac.jp>
2024-06-24 23:03:45 +0900

    ABC359

--- ABC/A.cpp
+++ ABC/A.cpp
@@ -3,26 +3,10 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 	string s;
 	cin >> s;
 	int d = s.length();
 	while (s.at(d - 1) != '.') d--;
 	cout << s.substr(d) << endl;
-=======
-	int N;
-	cin >> N;
-	vector<string> S(N);
-	int            count = 0;
-	for (int i = 0; i < N; i++)
-	{
-		cin >> S.at(i);
-		if (S.at(i) == "Takahashi")
-		{
-			count++;
-		}
-	}
-	cout << count << endl;
->>>>>>> 9ac7a88a00e2eb591ebb03102ce23683fa9211ad
 	return 0;
 }
--- ABC/B.cpp
+++ ABC/B.cpp
@@ -3,26 +3,7 @@ using namespace std;
 using ll = long long;
 int main()
 {
-<<<<<<< HEAD
 	ll H, W, N;
 	cin >> H >> W >> N;
-=======
-	int N;
-	cin >> N;
-	vector<int> A(2 * N);
-	for (int i = 0; i < 2 * N; i++)
-	{
-		cin >> A.at(i);
-	}
-	int count = 0;
-	for (int i = 0; i < 2 * N; i++)
-	{
-		if (A[i] == A[i + 2])
-		{
-			count++;
-		}
-	}
-	cout << count << endl;
->>>>>>> 9ac7a88a00e2eb591ebb03102ce23683fa9211ad
 	return 0;
 }
