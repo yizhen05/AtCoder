aca4d86606fd161132403c14dad05b4c20cce5c0
yizhen05 <s22056@tokyo.kosen-ac.jp>
2024-07-06 22:39:04 +0900

    ABC361 250点B問題8分で解けたの天才すぎじゃない？

--- ABC/A.cpp
+++ ABC/A.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+using ll = long long;
 int main()
 {
-	string s;
-	cin >> s;
-	if (s[2] != 'R' && s[0] != 'M')
-	{
-		cout << "Yes" << endl;
-	}
-	else
-	{
-		cout << "No" << endl;
-	}
+	int N, K, X;
+	cin >> N >> K >> X;
+	vector<int> A(N);
+	for (int i = 0; i < N; i++) cin >> A[i];
+	vector<int> B = A;
+	B.insert(B.begin() + K, X);
+	for (int i : B) std::cout << i << " ";
+	std::cout << std::endl;
 	return 0;
 }
