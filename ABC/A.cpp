85fe0bf52d3d4bd9bf1e554b931966e4da013ffc
yizhen05 <s22056@tokyo.kosen-ac.jp>
2024-07-15 09:34:14 +0900

    ABC361
    githubわからん

--- ABC/A.cpp
+++ ABC/A.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 	int    R, G, B;
 	string C;
 	cin >> R >> G >> B;
@@ -14,15 +13,5 @@ int main()
 		cout << min(R, B) << endl;
 	else
 		cout << min(R, G) << endl;
-=======
-	int N, K, X;
-	cin >> N >> K >> X;
-	vector<int> A(N);
-	for (int i = 0; i < N; i++) cin >> A[i];
-	vector<int> B = A;
-	B.insert(B.begin() + K, X);
-	for (int i : B) std::cout << i << " ";
-	std::cout << std::endl;
->>>>>>> 9352d102bdddec1865bb613d777b669a384ef9b5
 	return 0;
 }
--- ABC/B.cpp
+++ ABC/B.cpp
@@ -3,29 +3,8 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
 	int xa, ya, xb, yb, xc, yc;
 	cin >> xa >> ya >> xb >> yb >> xc >> yc;
-=======
-	int a, b, c, d, e, f;
-	int g, h, i, j, k, l;
-
-	cin >> a >> b >> c >> d >> e >> f;
-	cin >> g >> h >> i >> j >> k >> l;
-
-	bool x = max(a, g) < min(d, j);
-	bool y = max(b, h) < min(e, k);
-	bool z = max(c, i) < min(f, l);
-
-	if (x && y && z)
-	{
-		cout << "Yes" << endl;
-	}
-	else
-	{
-		cout << "No" << endl;
-	}
->>>>>>> 9352d102bdddec1865bb613d777b669a384ef9b5
 
 	double AB2 = pow(xb - xa, 2) + pow(yb - ya, 2);
 	double BC2 = pow(xc - xb, 2) + pow(yc - yb, 2);
